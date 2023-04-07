7118235fd92152d33a557b35efaf3b911ab166bd
Dmitry <dmitrys1808@yandex.ru>
2023-04-07 11:07:22 +0300

    secondtry

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,5 @@
 let editButton = document.querySelector('.profile__edit-button');
 let addButton = document.querySelector('.profile__add-button');
-let saveButton = document.querySelector('.popup__button');
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__icon');
 let formElement = document.querySelector('.edit-form');
@@ -9,7 +8,9 @@ let jobInput = document.querySelector('#description');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 function openPopup() {
-    popup.classList.add('popup_opened')
+    popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileDescription.textContent;
 }
 function closePopup(){
     popup.classList.remove('popup_opened')
@@ -18,10 +19,8 @@ editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 function handleFormSubmit (evt) {
     evt.preventDefault();
-let name = nameInput.value;
-let description = jobInput.value;
-profileName.textContent = name;
-profileDescription.textContent = description;
+profileName.textContent = nameInput.value;
+profileDescription.textContent = jobInput.value;
 closePopup()
 }
 formElement.addEventListener('submit', handleFormSubmit);
